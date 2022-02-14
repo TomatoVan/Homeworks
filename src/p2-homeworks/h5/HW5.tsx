f09a0a7da8c04664f3e887387ebbe51450175dd3
Leo <kovzel.leonid@mail.ru>
2022-02-14 22:40:52 +0300

    Fixed routing

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import t from "../../p1-main/m1-ui/u1-app/App.module.css";
-import {BrowserRouter} from "react-router-dom";
 import AllRoutes from "./Routes";
 
 function HW5() {
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
-			<div className={t.wrapper}>
-
-				<Header/>
-
-				<AllRoutes/>
-
-			</div>
-		</BrowserRouter>
+		<div className={t.wrapper}>
+			<Header/>
+			<AllRoutes/>
+		</div>
 	)
 }
 
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -16,11 +16,11 @@ const AllRoutes = () => {
 	return (
 		<div>
 			<Routes>
-				<Route path={"/"} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+				<Route path={"/"} element={<Navigate replace to={PATH.PRE_JUNIOR}/>}/>
 				<Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
 				<Route path={PATH.JUNIOR} element={<Junior/>}/>
 				<Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-				<Route path="*" element={<Error404/>}/>
+				<Route path="/*" element={<Error404/>}/>
 			</Routes>
 		</div>
 	)
